a7e278298c04344a056f2bce50f8a6d3c0f99157
whyujjwal <its.ujjwalrajhere@gmail.com>
2024-09-15 17:31:28 +0530

    commit

--- utils/db.py
+++ utils/db.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "postgres://avnadmin:AVNS_sSwPwLTEupO0arewyvZ@pg-1eaecb39-smartqna-be.c.aivencloud.com:18312/pg-1eaecb39-smartqna-be.c.aivencloud.com" #put in env variable later on
-DATABASE_URL = "sqlite:///./smartqna.db" 
+
 
 # engine = create_engine(DATABASE_URL)
 
